357c912d9479644934c935ed23482651ffe9f59a
Jesús López <jeslopalo@sandbox.es>
2016-01-30 00:25:18 +0100

    Add spring-boot skeleton

---
+++ src/main/java/es/sandbox/app/photomaniac/PhotomaniacApplication.java
@@ -0,0 +1,12 @@
+package es.sandbox.app.photomaniac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotomaniacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotomaniacApplication.class, args);
+	}
+}
---
+++ src/test/java/es/sandbox/app/photomaniac/PhotomaniacApplicationTests.java
@@ -0,0 +1,16 @@
+package es.sandbox.app.photomaniac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PhotomaniacApplication.class)
+public class PhotomaniacApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
